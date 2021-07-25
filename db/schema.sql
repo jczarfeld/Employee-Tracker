a151051b2d11401bd79ae35730dae7b89d522d1d
John Osterfeld <jczarfeld@gmail.com>
2021-07-25 15:22:37 -0400

    started filling out some tester data for tables and I believe my schema table creators are ready

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,57 @@
 DROP DATABASE IF EXISTS exployees;
 CREATE DATABASE employees;
 
-CREATE TABLE department (
--- define these tables
+USE employees;
 
+CREATE TABLE department (
+   id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+
+PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
 -- define these tables -- might have to change it from "role" because it's blue
+title VARCHAR(255) NOT NULL,
+ id INT NOT NULL, 
+ department_id INT NOT NULL,
+ department_name VARCHAR(255) NOT NULL,
+ salary INT NOT NULL,
+       
+
+       FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  FOREIGN KEY (department_name)
+  REFERENCES department(name)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE manager (
+    id INT NOT NULL, 
+    first_name VARCHAR(255) NOT NULL,
+     last_name VARCHAR(255) NOT NULL;
+     PRIMARY KEY(id)
 
 );
 
 CREATE TABLE employee (
+    id INT NOT NULL, 
+    first_name VARCHAR(255) NOT NULL,
+     last_name VARCHAR(255) NOT NULL,
+      role_title VARCHAR(255) NOT NULL,
+       department VARCHAR(255) NOT NULL,
+        salary INT NOT NULL,
+        manager_id INT NOT NULL,
+        role_id INT NOT NULL,
+        manager VARCHAR(255); 
+
+         FOREIGN KEY (manager_id)
+  REFERENCES manager(id)
+         FOREIGN KEY (role_id)
+  REFERENCES role(id)
+         FOREIGN KEY (role_title)
+  REFERENCES role(title)
+  ON DELETE SET NULL
 -- define these tables
 
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (department_id, department_name)
+VALUES (69, "Web Development"),
+       (420, "Data Science"),
+       (138, "Math"),
+       (840, "Electives"),
+       (99217, "Linear Algebra"),
+       (0213, "History of the Internet"),
+       (9026, "Machine Learning"),
+       (3298, "Game Design"),
+       (4278, "Cloud Development");
+
+INSERT INTO roles (job_title, role_id, department_name, salary)
+VALUES ("Manager", 678, "Web Development", 69000),
+       ("Engineer", 679, "Data Science", 20123),
+       ("Linear Algegratician", 6903, "Linear Algebra", 23125),
+       ("Historian", 5822, "History of the Internet", 94012),
+       ("Machinist", 84329, "Machine Learning", 654),
+       ("Matashomi Grasakowa", 69420, "Game Design", 12125 ),
+       ("God", 2, "Cloud Development", 910021);
+       
+       
+       INSERT INTO employees (id, first_name, last_name, job_title, department_name, salary, manager)
+VALUES (12, "James", "Miami", "Matashomi Grasakowa", "Game Design", 12125, "Bossy McCharge"),
+       (8492, "Bossy", "McCharge", "Manager", "Game Design", 24250),
+       (9034, "Matthew", "MarcLuke", "Intern", "Game Design",  3313, "Bossy McCharge"),
+       (13, "Master", "Miami", "Skydiver", "Game Design", 42069, "Bossy McCharge"),
+       (423, "Machine Learning Andrea", "Dotcom", "God", "Data Science" 01010101, "God's Dad");
+      
